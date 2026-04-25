161dc570992948fc5e1cf8c8c0ca6356f98ad6ef
mcbdigital2025 <mcbdigital.au@gmail.com>
2026-04-26 01:39:51 +1000

    Update proxy.ts
    Signed-off-by: mcbdigital2025 <mcbdigital.au@gmail.com>

--- proxy.ts
+++ proxy.ts
@@ -73,7 +73,7 @@ export function proxy(request: NextRequest) {
   "/createUpdateServiceStaffNotes",
   "/createUpdateServiceMileage",
   "/createUpdateIncidentReport",
-  "/createUpdateIncidentReport",
+  "/createUpdateServiceExpense",
 ];
 
   // const hasValidToken = token && !isTokenExpired(token);
